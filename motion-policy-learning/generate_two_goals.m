9e95f289aa0ae11b7e129d2037af97ef23d39bca
liangyuwei <liangyuwei@yahoo.com>
2019-06-24 18:12:46 +0800

    Tested diff_start_pose_diff_goal_pose, but it doesn't work well. Should try diff_start_pose_diff_goal_pose_same_goal_pos.

---
+++ motion-policy-learning/generate_two_goals.m
@@ -0,0 +1,11 @@
+function [goal_l, goal_r] = generate_two_goals(x_contact_origin, l_euler_final)
+
+
+l_x_final = x_contact_origin' + eul2rotm([l_euler_final(3), l_euler_final(2), l_euler_final(1)]) * [-0.12, 0, 0]';
+goal_l = [l_x_final', l_euler_final];
+
+r_euler_final = l_euler_final; r_euler_final(1:2) = -r_euler_final(1:2); r_euler_final(3) = pi + r_euler_final(3);
+r_x_final = x_contact_origin' + eul2rotm([r_euler_final(3), r_euler_final(2), r_euler_final(1)]) * [-0.19, 0, 0]';
+goal_r = [r_x_final', r_euler_final];
+
+end
