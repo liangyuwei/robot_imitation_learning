a9c09d829a8c5f5b990e0230d47e45effa244f7a
liangyuwei <liangyuwei@yahoo.com>
2020-07-17 21:36:13 +0800

    Adjusting visualization code...

--- dmp_relative_traj/visual_optimized_dmp_starts_and_goals.m
+++ dmp_relative_traj/visual_optimized_dmp_starts_and_goals.m
@@ -8,7 +8,7 @@ addpath('../vmp/'); % use resample_traj
 
 ori_file_name = '../motion-retargeting/test_imi_data_YuMi.h5';
 file_name = '../motion-retargeting/mocap_ik_results_YuMi_g2o_similarity.h5';
-group_name = 'kai_2';%'baozhu_1';%'fengren_1';
+group_name = 'fengren_1';%'kai_2';%'baozhu_1';%'fengren_1';
 
 num_datapoints = 50;
 
@@ -16,6 +16,9 @@ kP = 64; %Stiffness gain
 kV = (2*kP)^.5; %Damping gain (with ideal underdamped damping ratio)
 alpha = 1.0;
 dt = 0.04;
+
+max_round = h5read(file_name, ['/', group_name, '/max_round']); % for ease of display
+
 
 %% Prepare the original data for comparison
 time = h5read(ori_file_name, ['/', group_name, '/time']);
@@ -45,6 +48,7 @@ Weights_rw = h5read(ori_file_name, ['/', group_name, '/Weights_rw']);
 
 
 %% Load optimized DMP starts and goals and reproduce relative position trajectories using the optimized starts and goals 
+%{
 dmp_starts_goals = h5read(file_name, ['/', group_name, '/dmp_starts_goals_1']);
 new_goal_lrw = dmp_starts_goals(1:3);
 new_start_lrw = dmp_starts_goals(4:6);
@@ -60,7 +64,7 @@ y_lew = DMP_use_weights(Mu_lew, Sigma_lew, Weights_lew, 50, kP, kV, alpha, dt, n
 y_rew = DMP_use_weights(Mu_rew, Sigma_rew, Weights_rew, 50, kP, kV, alpha, dt, new_goal_rew, new_start_rew, false);
 y_rw = DMP_use_weights(Mu_rw, Sigma_rw, Weights_rw, 50, kP, kV, alpha, dt, new_goal_rw, new_start_rw, false);
 
-
+% plot relative trajs and rw
 %{
 figure; plot3(y_rw(1,:), y_rw(2,:), y_rw(3,:), 'r-.'); hold on; grid on;
 plot3(r_wrist_pos(1,:), r_wrist_pos(2,:), r_wrist_pos(3,:), 'b-.');
@@ -79,8 +83,7 @@ plot3(r_elbow_wrist_pos(1,:), r_elbow_wrist_pos(2,:), r_elbow_wrist_pos(3,:), 'b
 title('r\_elbow\_wrist\_pos');
 %}
 
-
-%% Obtain trajectories for body parts
+% Obtain trajectories for body parts
 y_r_wrist = y_rw;
 y_l_wrist = y_rw + y_lrw;
 y_r_elbow = y_rw + y_rew;
@@ -101,11 +104,11 @@ view(-45, 45);
 % title('Original and reproduced trajectories');
 title('Original and generalized trajectories');
 xlabel('x'); ylabel('y'); zlabel('z'); 
-
+%}
 
 
 %% Plot the cost history
-per_iteration = 5;
+per_iteration = 1;
 wrist_pos_cost_history = h5read(file_name, ['/', group_name, '/wrist_pos_cost_history']);
 l_wrist_pos_cost_history = h5read(file_name, ['/', group_name, '/l_wrist_pos_cost_history']);
 r_wrist_pos_cost_history = h5read(file_name, ['/', group_name, '/r_wrist_pos_cost_history']);
@@ -122,16 +125,16 @@ dmp_scale_cost_history = h5read(file_name, ['/', group_name, '/dmp_scale_cost_hi
 dmp_orien_cost_history = h5read(file_name, ['/', group_name, '/dmp_orien_cost_history']);
 dmp_rel_change_cost_history = h5read(file_name, ['/', group_name, '/dmp_rel_change_cost_history']);
 
-
 % jacobians for DMP starts and goals
 sim_jacobian_history = h5read(file_name, ['/', group_name, '/sim_jacobian_history']);
 track_jacobian_history = h5read(file_name, ['/', group_name, '/track_jacobian_history']);
 orien_jacobian_history = h5read(file_name, ['/', group_name, '/orien_jacobian_history']);
 scale_jacobian_history = h5read(file_name, ['/', group_name, '/scale_jacobian_history']);
+
 % update history of DMP
 dmp_update_history = h5read(file_name, ['/', group_name, '/dmp_update_history']);
 
-
+% plot all
 figure;
 plot((1:size(col_cost_history, 2))*per_iteration, sum(col_cost_history), 'b-'); hold on; grid on;
 plot((1:size(col_cost_history, 2))*per_iteration, sum(col_cost_history), 'bo');
@@ -210,6 +213,8 @@ plot((1:size(similarity_cost_history, 2))*per_iteration, similarity_cost_history
 title('History of similarity cost');
 xlabel('Iterations'); ylabel('Cost Value'); 
 
+
+%% Plot jacobian history
 % pre-processing before displaying jacobian
 dmp_starts_goals_dof = size(sim_jacobian_history, 1);
 num_records = size(sim_jacobian_history, 2);
@@ -282,6 +287,7 @@ dmp_starts_goals_moved = h5read(file_name, ['/', group_name, '/dmp_starts_goals_
 
 
 %% Analyze condition number of J'*J
+%{
 cond_nums = [];
 for i = 1 : size(track_jacobian_history, 2)
     J = track_jacobian_history(:, i); % column vector
@@ -289,11 +295,11 @@ for i = 1 : size(track_jacobian_history, 2)
 end
 disp(['History of condition numbers of J^T*J = ', num2str(cond_nums)]);
 
-
 % History of condition numbers of J^T*J = 7.388521923723018e+20  2.343177021191475e+20  3.929846188521127e+20  2.947066452778979e+20  3.816431692540564e+20  2.745309753057258e+22
+%}
 
 
-%% Debug: try to establish a constraint for DMP starts and goals
+%% Constraints related to DMP starts and goals
 % get goals and starts
 lrw_goal_ori = dmp_starts_goals_original(1:3);
 lrw_start_ori = dmp_starts_goals_original(4:6);
@@ -324,16 +330,29 @@ lew_vec_new = lew_goal_new - lew_start_new;
 rew_vec_new = rew_goal_new - rew_start_new;
 rw_vec_new = rw_goal_new - rw_start_new;
 
-lrw_ratio = norm(lrw_vec_new) / norm(lrw_vec_ori);
-lew_ratio = norm(lew_vec_new) / norm(lew_vec_ori);
-rew_ratio = norm(rew_vec_new) / norm(rew_vec_ori);
-rw_ratio = norm(rw_vec_new) / norm(rw_vec_ori);
+% constraints-related values
+lrw_ratio = norm(lrw_vec_new) / norm(lrw_vec_ori)
+lew_ratio = norm(lew_vec_new) / norm(lew_vec_ori)
+rew_ratio = norm(rew_vec_new) / norm(rew_vec_ori)
+rw_ratio = norm(rw_vec_new) / norm(rw_vec_ori)
 
-lrw_th = acos(lrw_vec_new'*lrw_vec_ori/norm(lrw_vec_new)/norm(lrw_vec_ori)) * 180 / pi;
-lew_th = acos(lew_vec_new'*lew_vec_ori/norm(lew_vec_new)/norm(lew_vec_ori)) * 180 / pi;
-rew_th = acos(rew_vec_new'*rew_vec_ori/norm(rew_vec_new)/norm(rew_vec_ori)) * 180 / pi;
-rw_th = acos(rw_vec_new'*rw_vec_ori/norm(rw_vec_new)/norm(rw_vec_ori)) * 180 / pi; % in degree !!
+lrw_th = acos(lrw_vec_new'*lrw_vec_ori/norm(lrw_vec_new)/norm(lrw_vec_ori)) * 180 / pi
+lew_th = acos(lew_vec_new'*lew_vec_ori/norm(lew_vec_new)/norm(lew_vec_ori)) * 180 / pi
+rew_th = acos(rew_vec_new'*rew_vec_ori/norm(rew_vec_new)/norm(rew_vec_ori)) * 180 / pi
+rw_th = acos(rw_vec_new'*rw_vec_ori/norm(rw_vec_new)/norm(rw_vec_ori)) * 180 / pi % in degree !!
 
+% Evaluate magnitudes of changes of relative DMPs
+lrw_goal_change = norm(lrw_goal_ori - lrw_goal_new)
+lrw_start_change = norm(lrw_start_ori - lrw_start_new)
+
+lew_goal_change = norm(lew_goal_ori - lew_goal_new)
+lew_start_change = norm(lew_start_ori - lew_start_new)
+
+rew_goal_change = norm(rew_goal_ori - rew_goal_new)
+rew_start_change = norm(rew_start_ori - rew_start_new)
+
+rw_goal_change = norm(rw_goal_ori - rw_goal_new)
+rw_start_change = norm(rw_start_ori - rw_start_new)
 
 
 %% Debug: analyze jacobian history
@@ -351,7 +370,6 @@ title('History of Jacobians w.r.t DMP starts and goals');
 xlabel('Iterations'); ylabel('Jacobians'); 
 legend([p1(1), p2(1), p3(1), p4(1)], 'orien\_jacobian', 'scale\_jacobian', 'track\_jacobian', 'sim\_jacobian', 'Location', 'NorthEastOutside');
 % 'track_jacobian', 'sim_jacobian', 'Location', 'NorthEastOutside');
-
 
 % norms
 num_records = size(track_jacobian_history, 2);
@@ -375,12 +393,31 @@ xlabel('Iterations'); ylabel('Norms of jacobians');
 legend([p1, p2, p3, p4], 'orien\_jacobian', 'scale\_jacobian', 'track\_jacobian', 'sim\_jacobian', 'Location', 'NorthEastOutside');
 
 
-%% Pre-iteration results - wrist and elbow's actual(executed) position trajectories
-% load pre-iteration results - wrist and elbow's position trajectories
-preiter_l_wrist_pos_traj = h5read(file_name, ['/', group_name, '/preiter_l_wrist_pos_traj']);
-preiter_r_wrist_pos_traj = h5read(file_name, ['/', group_name, '/preiter_r_wrist_pos_traj']);
-preiter_l_elbow_pos_traj = h5read(file_name, ['/', group_name, '/preiter_l_elbow_pos_traj']);
-preiter_r_elbow_pos_traj = h5read(file_name, ['/', group_name, '/preiter_r_elbow_pos_traj']);
+%% Optimization results of first round (q optimization -> manually move DMP starts and goals -> DMP optimization)
+% 1 - load initial DMP trajs
+dmp_starts_goals_initial = h5read(file_name, ['/', group_name, '/dmp_starts_goals_initial']);
+
+new_goal_lrw = dmp_starts_goals(1:3); new_start_lrw = dmp_starts_goals(4:6);
+new_goal_lew = dmp_starts_goals(7:9); new_start_lew = dmp_starts_goals(10:12);
+new_goal_rew = dmp_starts_goals(13:15); new_start_rew = dmp_starts_goals(16:18);
+new_goal_rw = dmp_starts_goals(19:21); new_start_rw = dmp_starts_goals(22:24);
+
+y_lrw = DMP_use_weights(Mu_lrw, Sigma_lrw, Weights_lrw, 50, kP, kV, alpha, dt, new_goal_lrw, new_start_lrw, false);
+y_lew = DMP_use_weights(Mu_lew, Sigma_lew, Weights_lew, 50, kP, kV, alpha, dt, new_goal_lew, new_start_lew, false);
+y_rew = DMP_use_weights(Mu_rew, Sigma_rew, Weights_rew, 50, kP, kV, alpha, dt, new_goal_rew, new_start_rew, false);
+y_rw = DMP_use_weights(Mu_rw, Sigma_rw, Weights_rw, 50, kP, kV, alpha, dt, new_goal_rw, new_start_rw, false);
+
+y_r_wrist_initial = y_rw;
+y_l_wrist_initial = y_rw + y_lrw;
+y_r_elbow_initial = y_rw + y_rew;
+y_l_elbow_initial = y_l_wrist + y_lew;
+
+% 2 - load actual executed trajs after q optimization in the first round
+actual_l_wrist_pos_traj_0 = h5read(file_name, ['/', group_name, '/actual_l_wrist_pos_traj_0']);
+actual_r_wrist_pos_traj_0 = h5read(file_name, ['/', group_name, '/actual_r_wrist_pos_traj_0']);
+actual_l_elbow_pos_traj_0 = h5read(file_name, ['/', group_name, '/actual_l_elbow_pos_traj_0']);
+actual_r_elbow_pos_traj_0 = h5read(file_name, ['/', group_name, '/actual_r_elbow_pos_traj_0']);
+
 optimed_l_wrist_pos_traj = h5read(file_name, ['/', group_name, '/optimed_l_wrist_pos_traj']);
 optimed_r_wrist_pos_traj = h5read(file_name, ['/', group_name, '/optimed_r_wrist_pos_traj']);
 optimed_l_elbow_pos_traj = h5read(file_name, ['/', group_name, '/optimed_l_elbow_pos_traj']);
@@ -420,10 +457,10 @@ plot3(y_r_wrist(1, :), y_r_wrist(2, :), y_r_wrist(3, :), 'r--');
 plot3(y_l_elbow(1, :), y_l_elbow(2, :), y_l_elbow(3, :), 'r--'); 
 plot3(y_r_elbow(1, :), y_r_elbow(2, :), y_r_elbow(3, :), 'r--'); % initial target
 
-plot3(preiter_l_wrist_pos_traj(1, :), preiter_l_wrist_pos_traj(2, :), preiter_l_wrist_pos_traj(3, :), 'g--');
-plot3(preiter_r_wrist_pos_traj(1, :), preiter_r_wrist_pos_traj(2, :), preiter_r_wrist_pos_traj(3, :), 'g--'); 
-plot3(preiter_l_elbow_pos_traj(1, :), preiter_l_elbow_pos_traj(2, :), preiter_l_elbow_pos_traj(3, :), 'g--'); 
-plot3(preiter_r_elbow_pos_traj(1, :), preiter_r_elbow_pos_traj(2, :), preiter_r_elbow_pos_traj(3, :), 'g--');  % pre-iteration results
+plot3(actual_l_wrist_pos_traj_0(1, :), actual_l_wrist_pos_traj_0(2, :), actual_l_wrist_pos_traj_0(3, :), 'g--');
+plot3(actual_r_wrist_pos_traj_0(1, :), actual_r_wrist_pos_traj_0(2, :), actual_r_wrist_pos_traj_0(3, :), 'g--'); 
+plot3(actual_l_elbow_pos_traj_0(1, :), actual_l_elbow_pos_traj_0(2, :), actual_l_elbow_pos_traj_0(3, :), 'g--'); 
+plot3(actual_r_elbow_pos_traj_0(1, :), actual_r_elbow_pos_traj_0(2, :), actual_r_elbow_pos_traj_0(3, :), 'g--');  % pre-iteration results
 
 view(-45, 45);
 % title('Original and reproduced trajectories');
@@ -438,10 +475,10 @@ plot3(y_r_wrist(1, :), y_r_wrist(2, :), y_r_wrist(3, :), 'r--');
 plot3(y_l_elbow(1, :), y_l_elbow(2, :), y_l_elbow(3, :), 'r--'); 
 plot3(y_r_elbow(1, :), y_r_elbow(2, :), y_r_elbow(3, :), 'r--'); % initial target
 
-plot3(preiter_l_wrist_pos_traj(1, :), preiter_l_wrist_pos_traj(2, :), preiter_l_wrist_pos_traj(3, :), 'g--');
-plot3(preiter_r_wrist_pos_traj(1, :), preiter_r_wrist_pos_traj(2, :), preiter_r_wrist_pos_traj(3, :), 'g--'); 
-plot3(preiter_l_elbow_pos_traj(1, :), preiter_l_elbow_pos_traj(2, :), preiter_l_elbow_pos_traj(3, :), 'g--'); 
-plot3(preiter_r_elbow_pos_traj(1, :), preiter_r_elbow_pos_traj(2, :), preiter_r_elbow_pos_traj(3, :), 'g--');  % pre-iteration results
+plot3(actual_l_wrist_pos_traj_0(1, :), actual_l_wrist_pos_traj_0(2, :), actual_l_wrist_pos_traj_0(3, :), 'g--');
+plot3(actual_r_wrist_pos_traj_0(1, :), actual_r_wrist_pos_traj_0(2, :), actual_r_wrist_pos_traj_0(3, :), 'g--'); 
+plot3(actual_l_elbow_pos_traj_0(1, :), actual_l_elbow_pos_traj_0(2, :), actual_l_elbow_pos_traj_0(3, :), 'g--'); 
+plot3(actual_r_elbow_pos_traj_0(1, :), actual_r_elbow_pos_traj_0(2, :), actual_r_elbow_pos_traj_0(3, :), 'g--');  % pre-iteration results
 
 view(-45, 45);
 % title('Original and reproduced trajectories');
@@ -569,16 +606,6 @@ title('Original, desired and pre-iteration optimized trajectories');
 xlabel('x'); ylabel('y'); zlabel('z'); 
 %}
 
-%% Evaluate magnitudes of changes of relative DMPs
-lrw_goal_change = norm(lrw_goal_ori - lrw_goal_new);
-lrw_start_change = norm(lrw_start_ori - lrw_start_new);
 
-lew_goal_change = norm(lew_goal_ori - lew_goal_new);
-lew_start_change = norm(lew_start_ori - lew_start_new);
 
-rew_goal_change = norm(rew_goal_ori - rew_goal_new);
-rew_start_change = norm(rew_start_ori - rew_start_new);
-
-rw_goal_change = norm(rw_goal_ori - rw_goal_new);
-rw_start_change = norm(rw_start_ori - rw_start_new);
 
